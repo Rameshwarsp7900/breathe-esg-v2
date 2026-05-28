12c6ae13bf6be0f77a5eb0829bbbdd97c129ab55
Rameshwarsp7900 <rameshwarsp7900@gmail.com>
2026-05-28 18:03:34 +0530

    Fix static files storage and add collectstatic to start command

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -76,7 +76,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static & Media
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
